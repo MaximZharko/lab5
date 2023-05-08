9d4d8c7c63ddb77d2584cec04eda517ca21bab6e
MaximZharko <fogvilka@gmail.com>
2023-05-08 23:56:56 +0300

    second commit

--- 5.1.py
+++ 5.1.py
@@ -3,7 +3,11 @@ list_of_nums = [1, 2, 3, 4, 5]
 print("Введите произвольное число")
 num = int(input())
 if num in list_of_nums:
+    print(*list_of_nums)
+    print(num)
     print("Поздравляю, Вы угадали число!")
 else:
+    print(*list_of_nums)
+    print(num)
     print("Нет такого числа!")
 
